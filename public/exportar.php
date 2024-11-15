0bc3849dec17c0ab766491cdc7c6083e782c31b1
JonathanRobinsonAristizabalSoto <jonathan_aristizabal@soy.sena.edu.co>
2024-11-15 17:31:01 -0500

    Actualizacion exportar

--- public/exportar.php
+++ public/exportar.php
@@ -1,5 +1,5 @@
 <?php
-require 'vendor/autoload.php';
+require '../vendor/autoload.php';
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
@@ -91,7 +91,7 @@ $writer = new Xlsx($spreadsheet);
 $filename = 'datos_seguridad_laboral.xlsx';
 $writer->save($filename);
 
-// Descargar el archivo excel
+// Descargar el archivo
 header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
 header('Content-Disposition: attachment; filename="' . $filename . '"');
 header('Content-Length: ' . filesize($filename));
